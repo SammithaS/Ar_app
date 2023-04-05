aeb8eb48bb5b2961df95df3232f181a2c0254c20
SammithaS <121117205+SammithaS@users.noreply.github.com>
2023-04-05 21:34:35 +0530

    Add files via upload

---
+++ java/com/example/arfashion/loginactivity.java
@@ -0,0 +1,17 @@
+package com.example.arfashion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+
+public class loginactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginactivity);
+
+
+    }
+}
